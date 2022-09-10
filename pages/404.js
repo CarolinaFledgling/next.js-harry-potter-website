e754b041adb2f8eaf04eb6551fba432c8bb46d7f
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-10 15:37:27 +0200

    added markup and style for 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotExist = () => {
+    return (
+        <div className="main-notFound">
+            <div className="fof">
+                <h3>Upss...We didn&apos;t find your page </h3>
+            </div>
+        </div>
+    )
+}
+
+export default NotExist
